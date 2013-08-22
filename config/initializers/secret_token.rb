ea17af56964b64f89ff5e7ae31fabfc0df0fb2f7
pedrocarmona <pcarmona1990@gmail.com>
2013-08-22 17:34:55 +0100

    added untracked files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Printandget::Application.config.secret_key_base = '29d859b8fe1f8f115b448a5a4ee367c9f0e34894273b549b175a75833824675071005e46ff5a4ec7157d421554cf5e84eb70779147d106d012b747bd2e22f150'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Printandget::Application.config.session_store :cookie_store, key: '_printandget_session'
